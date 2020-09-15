ad0e82368480c38a3c0bfb03ceeb6425dddbb727
odysz <odysseusj@163.com>
2020-09-16 00:37:14 +0800

    bad logic

--- normode/core/src/io/oz/wnw/ecs/sys/SysAffine.java
+++ normode/core/src/io/oz/wnw/ecs/sys/SysAffine.java
@@ -1,19 +1,30 @@
 package io.oz.wnw.ecs.sys;
 
+import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
 
 import io.oz.wnw.ecs.cmp.Affine;
+import io.oz.wnw.ecs.cmp.Obj3;
 
 public class SysAffine extends IteratingSystem {
 
+	private ComponentMapper<Affine> mAffine;
+	private ComponentMapper<Obj3> mObj3;
+
 	public SysAffine() {
-		super(Family.all(Affine.class).get());
+		super(Family.all(Affine.class, Obj3.class).get());
+		mAffine = ComponentMapper.getFor(Affine.class);
+		mObj3 = ComponentMapper.getFor(Obj3.class);
 	}
 
 	@Override
 	protected void processEntity(Entity entity, float deltaTime) {
+		Affine a = mAffine.get(entity);
+		Obj3 obj = mObj3.get(entity);
+		ModelInstance mi = obj.modInst;
 	}
 
 }
